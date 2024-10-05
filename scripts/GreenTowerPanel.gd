9c8bddddeafe49d7b3518504d62ed771c9abdd62
TyNiemeier <sctythefly@gmail.com>
2024-10-05 10:53:02 -0400

    added new tower

--- scripts/GreenTowerPanel.gd
+++ scripts/GreenTowerPanel.gd
@@ -1,18 +1,17 @@
-extends Panel
-class_name TowerPanel
+extends TowerPanel
 
-@export var tower : PackedScene
-var currTile
+
+
 
 func _on_gui_input(event):
-	var tempTower = tower.instantiate()
+	var tempTower = tower
 	if event is InputEventMouseButton and event.button_mask == 1:
 		#Left button down
 		add_child(tempTower)
 
-		tempTower.process_mode = Node.PROCESS_MODE_DISABLED
+		#tempTower.process_mode = Node.PROCESS_MODE_DISABLED
 
-		tempTower.scale = Vector2(0.32,0.32)
+		#tempTower.scale = Vector2(0.32,0.32)
 
 	elif event is InputEventMouseButton and event.button_mask == 1:
 		#left click down drag
@@ -21,9 +20,10 @@ func _on_gui_input(event):
 	elif event is InputEventMouseButton and event.button_mask == 0:
 		#left click up
 		
-		get_child(1).queue_free()
+		#get_child(1).queue_free()
 		var path = get_tree().get_root().get_node("Main/Towers")
 
 		path.add_child(tempTower)
-		tempTower.global_position = get_global_mouse_position()
-		tempTower.get_node("Panel").hide()
+		#tempTower.global_position = get_global_mouse_position()
+		#tempTower.get_node("Panel").hide()
+#everything is showing errors when it is the exact code as the working panel like everyhthing is showing as nule when it is connected to the parent
